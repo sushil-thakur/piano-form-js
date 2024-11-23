bf743c83477bc6c7f6496418136e3efe67aac943
sushil <sushilt2059@gmail.com>
2024-11-23 15:43:35 +0545

    complete

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 const pianoKeys = document.querySelectorAll(".key");
 const volumeSlider = document.getElementById("volume-slider");
-const toggleKeysCheckbox = document.getElementById("toggle-keys");
-let audio = new Audio();
+const keysCheckbox = document.getElementById("toggle-keys");
+let allKeys = [],
+    audio = new Audio();
 
 const playTune = (key) => {
     audio.src = `audio/${key}.wav`; // Set audio source
     audio.play(); // Play audio
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150); // Add playing class to key
 };
 
 // Add click event listeners to each key
 pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value / 100; // Adjust volume to 0-1 range
+};
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => {
+        key.classList.toggle("hide");
+    });
+};
+
+const pressedKeys = (e) => {
+   if (allKeys.includes(e.key)) playTune(e.key);
+};
+
+document.addEventListener("keydown", pressedKeys);
+volumeSlider.addEventListener("input", handleVolume);
+keysCheckbox.addEventListener("click", showHideKeys);
